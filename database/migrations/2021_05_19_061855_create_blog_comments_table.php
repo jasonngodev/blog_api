538c93ff6511206788776a9a224129d220ba694a
JasonNgo <thangloi@iMac-cua-TLG.local>
2021-05-19 15:05:02 +0700

    Laravel API

---
+++ database/migrations/2021_05_19_061855_create_blog_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('comment');
+            $table->integer('blog_post_id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_comments');
+    }
+}
